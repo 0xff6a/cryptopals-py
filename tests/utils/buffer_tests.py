3732c7551ca25e407c2b965bc1552fb8199d3750
Jeremy Fox <jeremyfox@The-Fox.local>
2016-03-16 17:14:20 +0000

    filling out buffer class

--- tests/utils/buffer_tests.py
+++ tests/utils/buffer_tests.py
@@ -8,22 +8,55 @@ def test_properties():
     assert_equal(buf.bytes, [1, 2, 3])
     assert_equal(buf.size, 3)
 
+def test_get():
+    buf = Buffer([0, 16, 0])
+
+    assert_equal(buf.get(1), 16)
+
+def test_set():
+    buf = Buffer([0, 0, 0, 0])
+    buf.set(1, 16)
+
+    assert_equal(buf.get(1), 16)
+
 def test_init():
     buf = Buffer.init(5, 16)
 
     assert_equal(buf.bytes, [16, 16, 16, 16, 16])
 
-def test_to_base64():
+def test_to_b64():
     buf = Buffer('base64 test')
 
-    assert_equal(buf.to_base64(), 'YmFzZTY0IHRlc3Q=')
+    assert_equal(buf.to_b64(), 'YmFzZTY0IHRlc3Q=')
+
+def test_from_b64():
+    buf = Buffer.from_b64('aGVsbG8=')
+
+    assert_equal(buf.to_string(), 'hello')
 
 def test_to_hex():
     buf = Buffer('hex test')
 
     assert_equal(buf.to_hex(), '6865782074657374')
 
+def test_from_hex():
+    buf = Buffer.from_hex('68656c6c6f')
+
+    assert_equal(buf.to_string(), 'hello')
+
 def test_to_bin():
     buf = Buffer('Az')
 
     assert_equal(buf.to_bin(), '0100000101111010')
+
+def test_from_bin():
+    buf = Buffer.from_bin('0100000101111010')
+
+    assert_equal(buf.to_string(), 'Az')
+
+def test_xor():
+    buf1 = Buffer('abc')
+    buf2 = Buffer('   ')
+    xord = buf1.xor(buf2)
+
+    assert_equal(xord.to_string(), 'ABC')
--- utils/buffer.py
+++ utils/buffer.py
@@ -2,6 +2,8 @@ from array import array
 import base64
 import binascii
 
+SZ_BYTE = 8
+
 class Buffer(object):
     """"Generic buffer object to hold binary data for manipulation"""
     def __init__(self, data):
@@ -11,23 +13,47 @@ class Buffer(object):
     def init(_class, size, value = 0):
         """Initialize a buffer of the specified size initialized to value"""
         data = [value] * size
+
         return _class(data)
 
     @classmethod
-    def from_base64(_class, string):
+    def from_b64(_class, string):
         """Create a new buffer from a base64 encoded string"""
+        ascii_str = base64.b64decode(string)
+
+        return _class(ascii_str)
 
     @classmethod
     def from_hex(_class, string):
         """Create a new buffer from a hex encoded string"""
+        ascii_str = string.decode('hex')
+
+        return _class(ascii_str)
 
     @classmethod
     def from_bin(_class, string):
         """Create a new buffer from a binary encoded string"""
+        ascii_str = ''
+        for i in xrange(0, len(string), SZ_BYTE):
+            ascii_str += chr(int(string[i:i + SZ_BYTE], 2))
+
+        return _class(ascii_str)
 
     @classmethod
-    def from_file(_class, filepath):
+    def from_file(_class, filepath, encoding):
         """Create a new buffer from the contents of a file"""
+        fo = file.open(filepath, 'r')
+        raw = fo.read()
+        fo.close()
+
+        if encoding == 'b64':
+            return _class.from_b64(raw)
+        elif encoding == 'hex':
+            return _class.from_hex(raw)
+        elif encoding == 'bin':
+            return _class.from_bin(raw)
+        else:
+            return _class(raw)
 
     @property
     def bytes(self):
@@ -39,11 +65,19 @@ class Buffer(object):
         """Returns the buffer size (bytes)"""
         return len(self.__data)
 
+    def get(self, index):
+        """Get the byte value at index"""
+        return self.__data[index]
+
+    def set(self, index, value):
+        """Set the byte value at index"""
+        self.__data[index] = value
+
     def to_string(self):
         """Converts a buffer to an ASCII string"""
         return self.__data.tostring()
 
-    def to_base64(self):
+    def to_b64(self):
         """Converts the buffer to a base64 encoded string"""
         return base64.b64encode(self.to_string())
 
@@ -59,12 +93,34 @@ class Buffer(object):
 
         return out_str
 
-    def to_file(self, encoding, filepath):
+    def to_file(self, filepath, encoding):
         """Dumps the buffer to a file in the specified encoding"""
+        if encoding == 'b64':
+            out_data = self.to_b64()
+        elif encoding == 'hex':
+            out_data = self.to_hex()
+        elif encoding == 'bin':
+            out_data = self.to_bin()
+        else:
+            out_data = self.to_string()
 
+        fo = open(filepath, 'w', 1)
+        fo.write(out_data)
+        fo.close()
+
+    def map(self, func):
+        """Map each byte of a buffer using supplied func"""
+        for byte in self.__data:
+            byte
 
     def xor(self, buffer):
         """Return XORed bytes of each buffer (up to size of current buffer)"""
+        result = self.__class__.init(self.size)
+
+        for i in range(self.size):
+            result.set(i, self.get(i) ^ buffer.get(i))
+
+        return result
 
     def concat(self, buffer):
         """Concatenate the buffer with another"""
